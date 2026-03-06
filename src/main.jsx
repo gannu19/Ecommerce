bcc9c843fb07c792e56807e8bcad88741b8c7a12
gannu19 <bhukyaganapathi00@gmail.com>
2026-03-06 15:40:17 +0530

    Move checkout page into react

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import {BrowserROuter} from 'react-router';
+import {BrowserRouter} from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
 
@@ -10,5 +10,5 @@ createRoot(document.getElementById('root')).render(
       <App />
     </BrowserRouter>
     
-  </StrictMode>,
+  </StrictMode>
 )
